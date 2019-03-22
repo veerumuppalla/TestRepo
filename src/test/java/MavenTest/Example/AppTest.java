b115c092c88e6362b35954d0ab7315ea32fa73bf
Veeru Muppalla <myemail@example.com>
2019-03-22 14:39:20 +0000

    First commit

---
+++ src/test/java/MavenTest/Example/AppTest.java
@@ -0,0 +1,46 @@
+package MavenTest.Example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+	App app = null;
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    
+    public void setUp() {
+    	
+    	if(app == null) {
+    		app = new App();
+    	}
+    }
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
