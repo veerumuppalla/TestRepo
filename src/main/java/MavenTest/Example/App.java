b2e8fb586c3c4d0c19a59a7e35eaa26eedf23fbd
veerumuppalla <veerumuppalla@gmail.com>
2021-03-07 17:19:33 +0000

    Update App.java

--- src/main/java/MavenTest/Example/App.java
+++ src/main/java/MavenTest/Example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
       System.out.println( "My First test Hello World!" );
-      System.out.println("Test local edit");
+      System.out.println("Test Remote edit");
     }
 }
