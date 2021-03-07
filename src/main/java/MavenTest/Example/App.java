42ae414081c0ab31c323b72095494b2d3f699ec8
veerumuppalla <veerumuppalla@gmail.com>
2021-03-07 17:12:44 +0000

    Update App.java

--- src/main/java/MavenTest/Example/App.java
+++ src/main/java/MavenTest/Example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "My First Hello World!" );
+        System.out.println( "My First test Hello World!" );
     }
 }
